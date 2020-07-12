bf36d8465f22fcd750148e1b89df01b5cacd7785
Timothy Cole <tim@timcole.me>
2020-07-11 23:37:26 -0700

    Added migration

--- src/postgres/connection.rs
+++ src/postgres/connection.rs
@@ -19,6 +19,66 @@ pub async fn setup() -> Result<Pool<PostgresConnectionManager<NoTls>>, RunError<
     Err(e) => panic!("builder error: {:?}", e),
   };
 
-  // TODO: Build migrations
+  let conn = pool.clone();
+  let conn = conn.get().await.unwrap();
+  match conn
+    .batch_execute(
+      "CREATE TABLE IF NOT EXISTS public.guilds (
+        id int8 NOT NULL,
+        name text,
+        owner_id int8,
+        region varchar(32),
+        splash varchar(255),
+        banner varchar(255),
+        icon varchar(255),
+        features _varchar DEFAULT '{}'::character varying[],
+        vanity varchar(255),
+        description text,
+        PRIMARY KEY (id)
+      );
+
+      CREATE TABLE IF NOT EXISTS public.members (
+        id int8 NOT NULL,
+        guild_id int8 NOT NULL,
+        avatar varchar(255),
+        bot bool,
+        name varchar(255),
+        discriminator int2,
+        roles _int8 NOT NULL DEFAULT '{}'::bigint[],
+        nick varchar(255),
+        PRIMARY KEY (id,guild_id)
+      );
+
+      CREATE TABLE IF NOT EXISTS public.messages (
+        id int8 NOT NULL,
+        guild_id int8 NOT NULL,
+        channel_id int8 NOT NULL,
+        author_id int8 NOT NULL,
+        content text NOT NULL,
+        permitted bool NOT NULL,
+        PRIMARY KEY (id,guild_id,channel_id)
+      );
+
+      CREATE TABLE IF NOT EXISTS public.roles (
+        id int8 NOT NULL,
+        colour varchar(6),
+        hoist bool,
+        mentionable bool,
+        name varchar(255),
+        permissions int8,
+        position int2,
+        guild_id int8 NOT NULL,
+        PRIMARY KEY (id,guild_id)
+      );
+
+      ALTER TABLE public.members ADD FOREIGN KEY (guild_id) REFERENCES public.guilds(id);
+      ALTER TABLE public.messages ADD FOREIGN KEY (guild_id) REFERENCES public.guilds(id);",
+    )
+    .await
+  {
+    Ok(_) => log::debug!("Ran Migrations"),
+    Err(e) => panic!("Failed to run migrations: {:?}", e),
+  };
+
   Ok(pool)
 }
